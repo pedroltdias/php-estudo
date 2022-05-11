ba12336e80c7c306c8cff294d0e51d8813e4376b
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-11 16:26:48 -0300

    Tipos primitivos

---
+++ tiposPrimitivos.php
@@ -0,0 +1,21 @@
+<?php
+
+//Dinamicamente Tipada
+
+//Inteiro - int
+$idade = 21;
+gettype($idade); //Integer
+
+//Decimais - double e float
+$salario = 1000.50;
+gettype($salario); //double || float
+
+//Texto - string
+$texto = "Olá, PHP!";
+gettype($texto); //string
+
+//Booleano - boolean
+$verdadeiro = true;
+gettype($verdadeiro); //boolean
+$falso = false;
+gettype($falso); //boolean
