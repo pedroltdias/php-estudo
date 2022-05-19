811665c44824344135e989927e0767770871d755
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-19 16:43:30 -0300

    php na web (forma antiga)

--- banco.php
+++ banco.php
@@ -16,12 +16,25 @@ $contasCorrentes = [
         'saldo' => 4000
     ]
 ];
+?>
 
-echo "<ul>";
-
-foreach($contasCorrentes as $chave => $conta) {
-
-    exibeConta($chave, $conta);
-}
-
-echo "</ul>";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Banco</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+    <dl>
+        <?php foreach ($contasCorrentes as $cpf=>$conta){?>
+        <dt>
+            <h3>Titular: <?= $conta['titular'];?> - CPF: <?=$cpf;?></h3>
+        </dt>
+        <dd>Saldo: <?= $conta['saldo']; ?></dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
