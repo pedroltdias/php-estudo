6e070e85f66bd27a18a22a1c87cdec3edf453797
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-19 16:26:31 -0300

    refatoracao do codigo para utilizar na web

--- banco.php
+++ banco.php
@@ -17,14 +17,6 @@ $contasCorrentes = [
     ]
 ];
 
-$contasCorrentes['120'] = sacar($contasCorrentes['120'], 500);
-
-$contasCorrentes['121'] = depositar($contasCorrentes['121'], 1000);
-
-unset($contasCorrentes[123]);
-
-titularComLetrasMaiusculas($contasCorrentes[120]);
-
 foreach($contasCorrentes as $chave => $conta) {
     ['titular' => $titular, 'saldo' => $saldo] = $conta;
     exibeMensagem("$chave => $titular, Saldo: $saldo");
