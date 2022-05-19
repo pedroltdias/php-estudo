2307710aaebacb464bff732c11850b944be460eb
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-19 14:46:36 -0300

    funcao depositar e tipagem de parametros e retorno

--- banco.php
+++ banco.php
@@ -1,14 +1,23 @@
 <?php
 
-function exibeMensagem($mensagem){
+function exibeMensagem(string $mensagem){
     echo $mensagem . PHP_EOL;
 }
 
-function sacar($conta, $valorASacar){
+function sacar(array $conta, float $valorASacar) : array {
     if ( $valorASacar > $conta['saldo']){
         exibeMensagem("Você não pode sacar um valor maior do que seu saldo!");
     } else {
         $conta['saldo'] -= 500;
+    }
+    return $conta;
+}
+
+function depositar(array $conta, float $valorADepositar) : array {
+    if ($valorADepositar > 0) {
+        $conta['saldo'] += $valorADepositar;
+    } else {
+        exibeMensagem("Você precisa depositar valores maiores que zero!");
     }
     return $conta;
 }
@@ -30,7 +39,9 @@ $contasCorrentes = [
 
 //$contasCorrentes['120']['saldo'] -= 500;  
 
-$contasCorrentes['120'] = sacar($contasCorrentes['120'], 1100);
+$contasCorrentes['120'] = sacar($contasCorrentes['120'], 500);
+
+$contasCorrentes['121'] = depositar($contasCorrentes['121'], 1000);
 
 foreach($contasCorrentes as $chave => $conta) {
     exibeMensagem($chave . " => " . $conta['titular'] . ", Saldo: " . $conta['saldo']);
