623b109a8e1912bc42ea95490efe20afcdc67e85
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-20 09:04:16 -0300

    funcoes 'krsort', 'ksort', 'arsort', 'rsort' e 'uksort'

---
+++ notas.php
@@ -0,0 +1,18 @@
+<?php
+
+$notas = [
+    'Maria'     => 9,
+    'Ana'       => 10,
+    'João'      => 8,
+    'Vinicius'  => 6,
+    'Roberto'   => 7,
+];
+
+
+// krsort($notas);      //Ordena pelas chaves em ordem alfabetica inversa
+// ksort($notas);       //Ordena pelas chaves em ordem alfabetica
+// arsort($notas);      //Ordena e Mantem as Chaves
+// rsort($notas);       //Inverte a ordem
+// uksort($notas);      //Ordena pelas chaves 
+
+var_dump($notas);
