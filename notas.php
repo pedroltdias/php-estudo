a14c1991dd7e1e88199c7e2dc8af42512885432a
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-20 09:35:55 -0300

    metodos para verificacao da existencia de elementos em um array

--- notas.php
+++ notas.php
@@ -4,7 +4,7 @@ $notas = [
     'Maria'     => 9,
     'Ana'       => 10,
     'João'      => 8,
-    'Vinicius'  => 6,
+    'Vinicius'  => null,
     'Roberto'   => 7,
 ];
 
@@ -21,4 +21,8 @@ if(is_array($notas)){
     echo "Sim, é um array" . PHP_EOL;
 }
 
-var_dump(array_is_list($notas));
+// var_dump(array_is_list($notas));
+
+var_dump(array_key_exists('Vinicius', $notas)); //verifica se um elemento ou chave existe no array
+
+var_dump(isset($notas['Vinicius'])); //verifica se um elemento ou chave existe no array e é diferente de nulo
