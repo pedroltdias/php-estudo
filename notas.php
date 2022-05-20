e36df2096994c323418ea4162566f2dd2a5409b2
Pedro Lucas Teixeira Dias <pedro.dias@sistemafibra.org.br>
2022-05-20 09:43:46 -0300

    busca em arrays pelo valor da chave

--- notas.php
+++ notas.php
@@ -17,12 +17,16 @@ $notas = [
 
 var_dump($notas);
 
-if(is_array($notas)){
-    echo "Sim, é um array" . PHP_EOL;
-}
+// if(is_array($notas)){
+//     echo "Sim, é um array" . PHP_EOL;
+// }
 
 // var_dump(array_is_list($notas));
 
-var_dump(array_key_exists('Vinicius', $notas)); //verifica se um elemento ou chave existe no array
+// var_dump(array_key_exists('Vinicius', $notas)); //verifica se um elemento ou chave existe no array
 
-var_dump(isset($notas['Vinicius'])); //verifica se um elemento ou chave existe no array e é diferente de nulo
+// var_dump(isset($notas['Vinicius'])); //verifica se um elemento ou chave existe no array e é diferente de nulo
+
+var_dump(in_array(10, $notas)); //verifica se existe o valor '10' no array -> converte valores
+
+echo array_search(10, $notas); //retorna a chave com valor == 10
