c892ea0ca68b2203aa75ffa3cdfd8b6372155385
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-25 17:15:41 -0300

    utilizando o comando htmlspecialchars para proteger a pagina

--- strings/cadastro.php
+++ strings/cadastro.php
@@ -21,15 +21,15 @@ $contato = new App\Alura\Contato($_POST['email'], $_POST['endereco'], $_POST['ce
 <body>
     <div class="mx-5 my-5">
         <h1>Cadastro feito com sucesso.</h1>
-        <p><?php echo $usuario->getTratamento(); ?>, seguem os dados de sua conta:</p>
+        <p><?php echo htmlspecialchars($usuario->getTratamento()); ?>, seguem os dados de sua conta:</p>
         <ul class="list-group">
-            <li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome(); ?></li>
-            <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome(); ?> </li>
-            <li class="list-group-item">Usuário: <?php echo $contato->getUsuario(); ?></li>
-            <li class="list-group-item">Senha: <?php echo $usuario->getSenha(); ?></li>
-            <li class="list-group-item">Telefone: <?php echo $contato->getTelefone(); ?></li>
-            <li class="list-group-item">Email: <?php echo $contato->getEmail(); ?></li>
-            <li class="list-group-item">Endereço: <?php echo $contato->getEnderecoCep(); ?></li>
+            <li class="list-group-item">Primeiro nome: <?php echo htmlspecialchars($usuario->getNome()); ?></li>
+            <li class="list-group-item">Sobrenome: <?php echo htmlspecialchars($usuario->getSobrenome()); ?> </li>
+            <li class="list-group-item">Usuário: <?php echo htmlspecialchars($contato->getUsuario()); ?></li>
+            <li class="list-group-item">Senha: <?php echo htmlspecialchars($usuario->getSenha()); ?></li>
+            <li class="list-group-item">Telefone: <?php echo htmlspecialchars($contato->getTelefone()); ?></li>
+            <li class="list-group-item">Email: <?php echo htmlspecialchars($contato->getEmail()); ?></li>
+            <li class="list-group-item">Endereço: <?php echo htmlspecialchars($contato->getEnderecoCep()); ?></li>
         </ul>
     </div>
 </body>
