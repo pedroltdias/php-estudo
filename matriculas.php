d88b03d44180c4152fc49653a031a9751c318b27
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-25 13:53:40 -0300

    unpacking/spread operator

--- matriculas.php
+++ matriculas.php
@@ -15,6 +15,10 @@ $novosAlunos = [
     'Nickson',
 ];
 
-$alunos = array_merge($antigosAlunos, $novosAlunos); //Não preserva as chaves antigas
+// $alunos = array_merge($antigosAlunos, $novosAlunos); //Não preserva as chaves antigas
+
+$alunos = [...$antigosAlunos, 'Pedro', ...$novosAlunos, 'Lucas']; //unpacking operator
+
+//unpacking == spread, a unica diferença é o contexto, mas o funcionamento é o mesmo
 
 var_dump($alunos);
