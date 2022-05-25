e6deed65fbeb728308c40f491310585788e0059b
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-25 15:08:14 -0300

    classe Usuario e refatoracao da divisao de nome e sobrenome

--- strings/Usuario.php
+++ strings/Usuario.php
@@ -1,8 +1,29 @@
 <?php
 
+namespace Alura;
+
 class Usuario
 {
-    
 
+    private $nome;
+    private $sobrenome;
+
+    public function __construct(string $nome)
+    {
+        $nomeSobrenome = explode(" ", $nome, 2);
+
+        $this->nome = $nomeSobrenome[0];
+        $this->sobrenome = $nomeSobrenome[1];
+    }
+
+    public function getNome() : string 
+    {
+        return $this->nome;
+    }
+
+    public function getSobrenome() : string
+    {
+        return $this->sobrenome;
+    }
 
 }
--- strings/cadastro.php
+++ strings/cadastro.php
@@ -1,3 +1,11 @@
+<?php
+
+require 'Usuario.php';
+
+$usuario = new \Alura\Usuario($_POST['nome']);
+
+?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,19 +18,12 @@
 </head>
 
 <body>
-
-<?php 
-    
-    $nomeSobrenome = explode(" ", $_POST['nome'], 2);
-
-?>
-
     <div class="mx-5 my-5">
         <h1>Cadastro feito com sucesso.</h1>
         <p>Seguem os dados de sua conta:</p>
         <ul class="list-group">
-            <li class="list-group-item">Primeiro nome: <?php echo $nomeSobrenome[0] ?></li>
-            <li class="list-group-item">Sobrenome: <?php echo $nomeSobrenome[1] ?> </li>
+            <li class="list-group-item">Primeiro nome: <?php echo $usuario->getNome() ?></li>
+            <li class="list-group-item">Sobrenome: <?php echo $usuario->getSobrenome() ?> </li>
             <li class="list-group-item">Usuário: </li>
             <li class="list-group-item">Senha: </li>
             <li class="list-group-item">Telefone: </li>
