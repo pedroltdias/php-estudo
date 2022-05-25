1e1478b5a47d46ebc2076aca14ceec2140e927b9
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-25 14:20:24 -0300

    compact e extract

--- tuplas.php
+++ tuplas.php
@@ -1,7 +1,15 @@
 <?php
 
-$dados = ['Vinicius', 10, 24];
+$dados = [
+    'nome'  => 'Vinicius', 
+    'nota'  => 10, 
+    'idade' => 24
+];
 // list($nome, $nota, $idade) = $dados;
-[$nome, $nota, $idade] = $dados;
+// [$nome, $nota, $idade] = $dados;
+
+extract($dados); //'extrai' chaves para variaveis
  
 var_dump($nome, $nota, $idade);
+
+var_dump(compact('nome', 'nota', 'idade')); //'compacta' variaveis separadas em um array
