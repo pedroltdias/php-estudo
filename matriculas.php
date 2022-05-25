e6a88cee3d193c7c933f25ecb675745959d4b4ee
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-25 14:03:03 -0300

    metodos de adicao e remocao de elementos em um array, tanto no comeco quanto no fim

--- matriculas.php
+++ matriculas.php
@@ -17,8 +17,17 @@ $novosAlunos = [
 
 // $alunos = array_merge($antigosAlunos, $novosAlunos); //Não preserva as chaves antigas
 
+//unpacking == spread, a unica diferença é o contexto, mas o funcionamento é o mesmo
+
 $alunos = [...$antigosAlunos, 'Pedro', ...$novosAlunos, 'Lucas']; //unpacking operator
 
-//unpacking == spread, a unica diferença é o contexto, mas o funcionamento é o mesmo
+array_push($alunos, 'Paulo', 'Andre'); //adicionar nomes ao final do array
+$alunos[] = 'Dias'; //adiciona nome ao final do array
+
+array_unshift($alunos, 'Joelma', 'Nathalia'); //adiciona nome no começo do array
+
+array_shift($alunos); //remove e retorna primeiro nome do array
+
+array_pop($alunos); //remove e retorna ultimo elemento do array
 
 var_dump($alunos);
