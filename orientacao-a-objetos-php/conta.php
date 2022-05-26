47b4b7ee1ea6dc2fdbc82d6dff452b3ec99ce920
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-26 13:49:17 -0300

    criacao do metodo publico sacar da classe conta

--- orientacao-a-objetos-php/conta.php
+++ orientacao-a-objetos-php/conta.php
@@ -5,4 +5,13 @@ class Conta
     public string $cpfTitular;      //é possível tipar ou não variaveis
     public string $nomeTitular;
     public float $saldo;
+
+    public function sacar(float $valorASacar)
+    {
+        if ($valorASacar > $this->$saldo) {
+            echo "Saldo indisponível!";
+        } else {
+            $this->saldo -= $valorASacar;
+        }
+    }
 }
