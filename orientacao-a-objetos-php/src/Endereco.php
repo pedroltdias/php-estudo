b090c8271b60885f617db7b4e51545b52dc3733f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-26 15:43:36 -0300

    classe Endereco, seus atributos, metodo construtor e getters

---
+++ orientacao-a-objetos-php/src/Endereco.php
@@ -0,0 +1,38 @@
+<?php
+
+class Endereco
+{
+    private string $cidade;
+    private string $bairro;
+    private string $rua;
+    private string $numero;
+
+    public function __construct(string $cidade, string $bairro, string $rua, string $numero)
+    {
+        $this->cidade = $cidade;
+        $this->bairro = $bairro;
+        $this->rua = $rua;
+        $this->numero = $numero;
+    }
+
+    public function getCidade() : string
+    {
+        return $this->cidade;
+    }
+
+    public function getBairro() : string
+    {
+        return $this->bairro;
+    }
+
+    public function getRua() : string
+    {
+        return $this->rua;
+    }
+
+    public function getNumero() : string
+    {
+        return $this->numero;
+    }
+
+}
