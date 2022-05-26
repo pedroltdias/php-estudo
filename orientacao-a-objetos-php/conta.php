d560c15b7716d35e10c9af6269e45b94eaa115ba
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-26 14:01:11 -0300

    refatoracao das validacoes dos metodos sacar, depositar e tranferir

--- orientacao-a-objetos-php/conta.php
+++ orientacao-a-objetos-php/conta.php
@@ -6,31 +6,31 @@ class Conta
     public string $nomeTitular;
     public float $saldo;
 
-    public function sacar(float $valorASacar)
+    public function sacar(float $valorASacar) : void
     {
         if ($valorASacar > $this->$saldo) {
             echo "Saldo indisponível!";
-        } else {
-            $this->saldo -= $valorASacar;
+            return;
         }
+        $this->saldo -= $valorASacar;
     }
 
     public function depositar(float $valorADepositar) : void
     {
         if ($valorADepositar < 0) {
             echo "Valor precisa ser positivo!";
-        } else {
-            $this->saldo += $valorADepositar;
+            return;
         }
+        $this->saldo += $valorADepositar;
     }
 
     public function transferir(float $valorATransferir, Conta $contaDestino) : void
     {
         if ($valorATransferir > $this->saldo) {
             echo "Saldo indisponível!";
-        } else {
-            $this->sacar($valorATransferir);
-            $contaDestino->depositar($valorATransferir);
+            return;
         }
+        $this->sacar($valorATransferir);
+        $contaDestino->depositar($valorATransferir);
     }
 }
