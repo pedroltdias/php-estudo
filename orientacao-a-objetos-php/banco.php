de3fd45eb7e8f2200f3c62866c8f3b51b8e406b7
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-30 17:06:47 -0300

    Correcao no caminho das classes utilizadas no arquivo banco.php

--- orientacao-a-objetos-php/banco.php
+++ orientacao-a-objetos-php/banco.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once 'src/Conta.php';
-require_once 'src/Titular.php';
-require_once 'src/CPF.php';
-require_once 'src/Endereco.php';
-require_once 'src/Pessoa.php';
+require_once 'src/Modelo/Conta/Conta.php';
+require_once 'src/Modelo/Conta/Titular.php';
+require_once 'src/Modelo/CPF.php';
+require_once 'src/Modelo/Endereco.php';
+require_once 'src/Modelo/Pessoa.php';
 
 $endereco = new Endereco('Brasilia', 'um bairro', 'minha rua', '27');
 $pedro = new Titular(new CPF('123.456.789-10'), 'Pedro Dias', $endereco);
