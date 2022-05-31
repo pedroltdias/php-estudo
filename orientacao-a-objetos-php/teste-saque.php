f2d143518d8c1a6a60210d2f7625319505fddbbe
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-05-31 16:53:46 -0300

    teste conta poupanca

--- orientacao-a-objetos-php/teste-saque.php
+++ orientacao-a-objetos-php/teste-saque.php
@@ -1,19 +1,19 @@
 <?php
 
 use Alura\Banco\Modelo\Conta\Conta;
+use Alura\Banco\Modelo\Conta\ContaPoupanca;
 use Alura\Banco\Modelo\Conta\Titular;
 use Alura\Banco\Modelo\CPF;
 use Alura\Banco\Modelo\Endereco;
 
 require_once 'autoload.php';
 
-$conta = new Conta(
+$conta = new ContaPoupanca(
     new Titular(
         new CPF('123.456.789-10'), 
         'Pedro Dias', 
         new Endereco('Brasilia', 'Bairro', 'Rua', '10')
-    ),
-    0
+    )
 );
 
 $conta->deposita(500);
