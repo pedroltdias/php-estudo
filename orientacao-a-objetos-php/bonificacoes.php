0ee2eec26006c269370c607d3a4c79155376d232
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-01 15:12:41 -0300

    teste das novas classes 'Desenvolvedor, Diretor e Gerente'

--- orientacao-a-objetos-php/bonificacoes.php
+++ orientacao-a-objetos-php/bonificacoes.php
@@ -2,25 +2,36 @@
 
 require_once 'autoload.php';
 
-use Alura\Banco\Modelo\{Funcionario, CPF};
+use Alura\Banco\Modelo\{CPF};
 use Alura\Banco\Service\ControladorDeBonificacoes;
+use Alura\Banco\Modelo\Funcionario\{Diretor, Funcionario, Gerente, Desenvolvedor};
 
-$umFuncionario = new Funcionario(
+$desenvolvedor = new Desenvolvedor(
     "Pedro Dias", 
     new CPF('123.456.789-10'), 
     'Desenvolvedor', 
     1000
 );
 
-$doisFuncionario = new Funcionario(
+$desenvolvedor->sobeDeNivel();
+
+$gerente = new Gerente(
     "Paulo Dias", 
     new CPF('987.654.321-10'), 
-    'Desenvolvedor', 
+    'Gerente', 
     3000
 );
 
+$diretor = new Diretor(
+    "Mauh Dias", 
+    new CPF('700.070.007-07'), 
+    'Diretor', 
+    5000
+);
+
 $controlador = new ControladorDeBonificacoes();
-$controlador->adicionaBonificacaoDe($umFuncionario);
-$controlador->adicionaBonificacaoDe($doisFuncionario);
+$controlador->adicionaBonificacaoDe($desenvolvedor);
+$controlador->adicionaBonificacaoDe($gerente);
+$controlador->adicionaBonificacaoDe($diretor);
 
 echo $controlador->recuperaTotal();
