155dcbea17e58b684a5c66bc8cf9397c0444d6a6
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-01 16:23:22 -0300

    remocao do atributo cargo da classe Funcionario, codigo adaptado para essa mudanca

--- orientacao-a-objetos-php/bonificacoes.php
+++ orientacao-a-objetos-php/bonificacoes.php
@@ -9,7 +9,6 @@ use Alura\Banco\Modelo\Funcionario\{Diretor, Funcionario, Gerente, Desenvolvedor
 $desenvolvedor = new Desenvolvedor(
     "Pedro Dias", 
     new CPF('123.456.789-10'), 
-    'Desenvolvedor', 
     1000
 );
 
@@ -18,14 +17,12 @@ $desenvolvedor->sobeDeNivel();
 $gerente = new Gerente(
     "Paulo Dias", 
     new CPF('987.654.321-10'), 
-    'Gerente', 
     3000
 );
 
 $diretor = new Diretor(
     "Mauh Dias", 
     new CPF('700.070.007-07'), 
-    'Diretor', 
     5000
 );
 
--- orientacao-a-objetos-php/src/Modelo/Funcionario/Funcionario.php
+++ orientacao-a-objetos-php/src/Modelo/Funcionario/Funcionario.php
@@ -6,29 +6,17 @@ use Alura\Banco\Modelo\{CPF, Pessoa};
 
 abstract class Funcionario extends Pessoa
 {
-    private string $cargo;
     private $salario;
 
-    public function __construct(string $nome, CPF $cpf, string $cargo, float $salario)
+    public function __construct(string $nome, CPF $cpf, float $salario)
     {
         parent::__construct($cpf, $nome);
-        $this->cargo = $cargo;
         $this->salario = $salario;
-    }
-
-    public function getCargo() : string 
-    {
-        return $this->cargo;
     }
 
     public function getSalario() : float
     {
         return $this->salario;
-    }
-
-    public function calculaBonificacao() : float
-    {
-        return $this->salario * 0.1;
     }
 
     public function recebeAumento(float $valorAumento) : void
