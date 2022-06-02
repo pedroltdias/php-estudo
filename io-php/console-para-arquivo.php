d1e650057c9c617c4bdb64e0cff2499374817d4e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-02 16:34:47 -0300

    forma facilitada e inteligentede ler arquivos do terminal e gravar em arquivo

--- io-php/console-para-arquivo.php
+++ io-php/console-para-arquivo.php
@@ -1,7 +1,5 @@
 <?php
 
-$teclado = fopen('php://stdin', 'r');
-
-$novoCurso = trim(fgets($teclado));
+$novoCurso = trim(fgets(STDIN));
 
 file_put_contents('cursos-php.txt', "\n$novoCurso", FILE_APPEND);
