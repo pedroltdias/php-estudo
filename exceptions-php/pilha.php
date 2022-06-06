d8bc6d866ef8676f4cefa2000b2d23b8c5d56669
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-06 14:20:39 -0300

    tratando exceptions em diferentes funcoes

--- exceptions-php/pilha.php
+++ exceptions-php/pilha.php
@@ -3,22 +3,23 @@
 function funcao1()
 {
     echo 'Inicio da funcao 1' . PHP_EOL;
-    
-    try{
-        $arrayFixo = new SplFixedArray(2);   
-        $arrayFixo[3] = 'Valor';
-    } catch (RuntimeException $problema) {
-        echo 'Aconteceu um erro na funcao 1' . PHP_EOL;
-    }
 
-    // $divisao = intdiv(5, 0); 
-    funcao2();
+    try{
+        funcao2();
+    } catch (RuntimeException $problema) {
+        echo '--> Funcao 1 resolvendo erro na funcao 2' . PHP_EOL;
+    }
     echo 'Fim da funcao 1' . PHP_EOL;
 }
 
 function funcao2()
 {
     echo 'Inicio da funcao 2' . PHP_EOL;
+
+    $arrayFixo = new SplFixedArray(2);   
+    $arrayFixo[3] = 'Valor';
+    // $divisao = intdiv(5, 0); 
+
     for ($i=0; $i <= 5; $i++) { 
         echo $i . PHP_EOL;
     }
