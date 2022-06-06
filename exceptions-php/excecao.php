a152b16c7cdf87bee47f39a594df04be527b7f35
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-06 15:06:40 -0300

    testando 'anotacoes' feitas pela IDE PHPStorm

--- exceptions-php/excecao.php
+++ exceptions-php/excecao.php
@@ -1,1 +1,19 @@
 <?php
+
+/**
+ * @throws Exception
+ */
+
+function funcaoQueLancaExcecao()
+{
+
+}
+
+/**
+ * @return void
+ * @throws Exception
+ */
+ function outraFuncao()
+ {
+     funcaoQueLancaExcecao();
+ }
