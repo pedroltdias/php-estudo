3823b1e3e7f888b054070aea8e1d693d9a079ad5
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-07 16:37:31 -0300

    criacao da classe Buscador

---
+++ buscador-cursos-alura/src/Buscador.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Alura\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+    /**
+     * @var ClientInterface
+     */
+    private $httpClient;
+    /**
+     * @var Crawler
+     */
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        $this->httpClient = $httpClient;
+        $this->crawler = $crawler;
+    }
+
+    public function buscar(string url): array
+    {
+        
+    }
+}
