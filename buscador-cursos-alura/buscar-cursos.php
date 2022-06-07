ea8846f2b53698743ecca5c240cd8f09f7b22ab9
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-07 16:46:32 -0300

    refatoracao do codigo do buscador e implementacao da funcao buscar()

--- buscador-cursos-alura/buscar-cursos.php
+++ buscador-cursos-alura/buscar-cursos.php
@@ -2,18 +2,15 @@
 
 require 'vendor/autoload.php';
 
+use Alura\BuscadorDeCursos\Buscador;
 use GuzzleHttp\Client;
 use Symfony\Component\DomCrawler\Crawler;
 
-$client = new Client(['verify' => false]);
-$resposta = $client->request('GET', 'https://www.alura.com.br/cursos-online-programacao/php');
-
-$html = $resposta->getBody();
-
+$client = new Client(['verify' => false, 'base_uri' =>'https://www.alura.com.br/' ]);
 $crawler = new Crawler();
-$crawler->addHtmlContent($html);
 
-$cursos = $crawler->filter('span.card-curso__nome');
+$buscador = new Buscador($client, $crawler);
+$cursos = $buscador->buscar('/cursos-online-programacao/php');
 
 foreach ($cursos as $curso) {
     echo $curso->textContent . PHP_EOL;
--- buscador-cursos-alura/src/Buscador.php
+++ buscador-cursos-alura/src/Buscador.php
@@ -22,8 +22,20 @@ class Buscador
         $this->crawler = $crawler;
     }
 
-    public function buscar(string url): array
+    public function buscar(string $url): array
     {
-        
+        $resposta = $this->httpClient->request('GET', $url);
+
+        $html = $resposta->getBody();
+        $this->crawler->addHtmlContent($html);
+
+        $elementosCursos = $this->crawler->filter('span.card-curso__nome');
+        $cursos = [];
+
+        foreach ($elementosCursos as $elemento) {
+            $cursos[] = $elemento->textContent;
+        }
+
+        return $cursos;
     }
 }
