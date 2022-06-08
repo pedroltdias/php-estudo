dda113b45557017400be5f9e228cd1936a9374d8
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-08 17:19:35 -0300

    reducao do codigo html utilizando php, no caso, array artigos e metodo foreach

--- webapp-php-e-mysql/index.php
+++ webapp-php-e-mysql/index.php
@@ -1,10 +1,23 @@
 <?php
 
-$artigo1 = [
-    'titulo' => 'Primeiros passos com Spring',
-    'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-    programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-    'link' => 'primeiros-passos-com-spring.html'
+$artigos = [
+    [
+        'titulo' => 'Primeiros passos com Spring',
+        'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
+        programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
+        'link' => 'primeiros-passos-com-spring.html'
+    ],
+    [
+        'titulo' => 'O que é Metodologia Ágil?',
+        'conteudo' => 'Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
+        'link' => 'o-que-e-metodologia-agil.html'
+    ],
+    [
+        'titulo' => 'Como é o funil do Growth Hacking?',
+        'conteudo' => 'Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
+        mais sobre ele.',
+        'link' => 'como-e-o-funil-do-growth-hacking.html'
+    ]
 ];
 
 ?>
@@ -20,31 +33,16 @@ $artigo1 = [
 <body>
     <div id="container">
         <h1>Meu Blog</h1>
+        <?php foreach($artigos as $artigo){?>
         <h2>
-            <a href="<?php echo $artigo1['link'] ?>">
-                <?php echo $artigo1['titulo']; ?>
+            <a href="<?php echo $artigo['link'] ?>">
+                <?php echo $artigo['titulo']; ?>
             </a>
         </h2>
         <p>
-            <?php echo $artigo1['conteudo']; ?>
+            <?php echo $artigo['conteudo']; ?>
         </p>
-        <h2>
-            <a href="o-que-e-metodologia-agil.html">
-                O que é Metodologia Ágil?
-            </a>
-        </h2>
-        <p>
-            Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.
-        </p>
-        <h2>
-            <a href="como-e-o-funil-do-growth-hacking.html">
-                Como é o funil do Growth Hacking?
-            </a>
-        </h2>
-        <p>
-            Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-            mais sobre ele.
-        </p>
+        <?php } ?>
     </div>
 </body>
 
