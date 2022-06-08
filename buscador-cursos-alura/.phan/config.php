dafcbc7767514dc49d53e3c16d47d17265206a1d
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-08 15:31:56 -0300

    scripts criados para rodar 'test', 'phpcs' e o 'phan'

---
+++ buscador-cursos-alura/.phan/config.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+
+    "target_php_version" => '8.1',
+
+    'directory_list' => [
+        'src',
+        'vendor/symfony/dom-crawler',
+        'vendor/guzzlehttp/guzzle',
+        'vendor/psr/http-message'
+    ],
+
+    "exclude_analysis_directory_list" => [
+        'vendor/'
+    ],
+
+    'plugins' => [
+        'AlwaysReturnPlugin',
+        'DollarDollarPlugin',
+        'DuplicateArrayKeyPlugin',
+        'DuplicateExpressionPlugin',
+        'PregRegexCheckerPlugin',
+        'PrintfCheckerPlugin',
+        'SleepCheckerPlugin',
+        'UnreachableCodePlugin',
+        'UseReturnValuePlugin',
+        'EmptyStatementListPlugin',
+        'LoopVariableReusePlugin',
+    ],
+];
