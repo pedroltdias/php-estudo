b6e806f536e2364da4f525532cd0fbe9e0d71cff
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-09 16:36:23 -0300

    metodo encontrarPorId

--- webapp-php-e-mysql/src/Artigo.php
+++ webapp-php-e-mysql/src/Artigo.php
@@ -19,4 +19,15 @@ class Artigo
 
         return $artigos;
     }
+
+    public function encontrarPorId(string $id)
+    {
+        $selecionaArtigo = $this->mysql->prepare("SELECT id, titulo, conteudo FROM artigos WHERE id = ?");
+
+        $selecionaArtigo->bind_param('s', $id);
+        $selecionaArtigo->execute();
+        $artigo = $selecionaArtigo->get_result()->fetch_assoc();
+
+        return $artigo;
+    }
 }
