2734e3ee34ce818b64b866c67a42ae155cff2d5f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-09 15:50:45 -0300

    refatoracao da condicao de exibicao do erro ao conectar com o bd

--- webapp-php-e-mysql/config.php
+++ webapp-php-e-mysql/config.php
@@ -3,9 +3,7 @@
 $mysql = new mysqli('localhost', 'root', '', 'blog');
 $mysql->set_charset('utf8');
 
-if ($mysql == TRUE) {
-    echo "Banco conectado";
-} else {
+if ($mysql == FALSE) {
     echo "Erro na conexão";
 }
 
