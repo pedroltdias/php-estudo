6fe11a350d057d7fc9c9f2a8874092e435c0ac5b
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-09 15:54:32 -0300

    criacao do arquivo 'artigo.php'

--- webapp-php-e-mysql/Artigo.php
+++ webapp-php-e-mysql/Artigo.php
@@ -1,22 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="pt-br">
 
-class Artigo
-{
+<head>
+    <title>Meu Blog</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
 
-    private $mysql;
+<body>
+    <div id="container">
+        <h1>
+            Primeiros passos com Spring
+        </h1>
+        <p>
+            Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
+            programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.
+        </p>
+        <div>
+            <a class="botao botao-block" href="index.html">Voltar</a>
+        </div>
+    </div>
+</body>
 
-    public function __construct(mysqli $mysql)
-    {
-        $this->mysql = $mysql;  
-    }
-
-    public function exibirTodos(): array
-    {
-
-        $resultado = $this->mysql->query('SELECT id, conteudo, titulo FROM artigos');
-
-        $artigos = $resultado->fetch_all(MYSQLI_ASSOC);
-
-        return $artigos;
-    }
-}
+</html>
