7bbe0a39336918800ede0d30286809acc6b1e193
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-09 15:49:36 -0300

    recebe valor dos arrays do banco / criacao do construtor da classe Artigo

--- webapp-php-e-mysql/Artigo.php
+++ webapp-php-e-mysql/Artigo.php
@@ -2,27 +2,20 @@
 
 class Artigo
 {
+
+    private $mysql;
+
+    public function __construct(mysqli $mysql)
+    {
+        $this->mysql = $mysql;  
+    }
+
     public function exibirTodos(): array
     {
-        $artigos = [
-            [
-                'titulo' => 'Primeiros passos com Spring',
-                'conteudo' => 'Na empresa onde trabalho começamos um Coding Dojo, que é basicamente uma reunião com programadores e
-                programadoras a fim de resolver desafios e aperfeiçoar as habilidades com algoritmos.',
-                'link' => 'primeiros-passos-com-spring.html'
-            ],
-            [
-                'titulo' => 'O que é Metodologia Ágil?',
-                'conteudo' => 'Uma vez fui contratada por uma empresa que desenvolvia softwares e aplicativos para outras empresas.',
-                'link' => 'o-que-e-metodologia-agil.html'
-            ],
-            [
-                'titulo' => 'Como é o funil do Growth Hacking?',
-                'conteudo' => 'Minha amiga que possui um clube de assinaturas começou a utilizar o Growth Hacking após conhecer um pouco
-                mais sobre ele.',
-                'link' => 'como-e-o-funil-do-growth-hacking.html'
-            ]
-        ];
+
+        $resultado = $this->mysql->query('SELECT id, conteudo, titulo FROM artigos');
+
+        $artigos = $resultado->fetch_all(MYSQLI_ASSOC);
 
         return $artigos;
     }
