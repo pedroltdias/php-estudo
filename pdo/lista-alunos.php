8b18a967e2a9a0fc7b04a3e53f1a6864c7e47329
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 10:48:39 -0300

    query para selecionar todos os alunos e listagem

--- pdo/lista-alunos.php
+++ pdo/lista-alunos.php
@@ -4,3 +4,6 @@ require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
+
+$statement = $pdo->query('SELECT * FROM students;');
+$studentList = $statement->fetchAll();
