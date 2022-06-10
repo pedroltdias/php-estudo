82c9fb70e2bd50ca5937f9c6cbee2967c13f755a
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 11:21:23 -0300

    utilizando o fetch() para buscar apenas um dado

--- pdo/lista-alunos.php
+++ pdo/lista-alunos.php
@@ -7,8 +7,20 @@ require_once 'vendor/autoload.php';
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
-$statement = $pdo->query('SELECT * FROM students;');
-$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$statement = $pdo->query('SELECT * FROM students WHERE id = 1;');
+
+while ($studentData = $statement->fetch(PDO::FETCH_ASSOC)) {
+    $student = new Student(
+        $studentData['id'],
+        $studentData['name'],
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+
+    echo $student->age() . PHP_EOL;
+}
+
+exit();
+
 $studentList = [];
 
 foreach ($studentDataList as $studentData) {
