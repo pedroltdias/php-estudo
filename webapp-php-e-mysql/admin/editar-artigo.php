2e6c4801980455e3a0e591d3c97babeb4302f360
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 09:26:36 -0300

    impressão do titulo e conteudo de artigos ao editar

--- webapp-php-e-mysql/admin/editar-artigo.php
+++ webapp-php-e-mysql/admin/editar-artigo.php
@@ -1,3 +1,15 @@
+<?php
+
+require '../config.php';
+include '../src/Artigo.php';
+require '../src/redireciona.php';
+
+$criaArtigo = new Artigo($mysql);
+$artigo = $criaArtigo->encontrarPorId($_GET['id']);
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="pt-br">
 
@@ -10,14 +22,14 @@
 <body>
     <div id="container">
         <h1>Editar Artigo</h1>
-        <form action="editar-artigo.html" method="post">
+        <form action="editar-artigo.php" method="post">
             <p>
                 <label for="titulo">Digite o novo título do artigo</label>
-                <input class="campo-form" type="text" name="titulo" id="titulo" value="" />
+                <input class="campo-form" type="text" name="titulo" id="titulo" value="<?php echo $artigo['titulo'] ?>" />
             </p>
             <p>
                 <label for="conteudo">Digite o novo conteúdo do artigo</label>
-                <textarea class="campo-form" type="text" name="conteudo" id="titulo"></textarea>
+                <textarea class="campo-form" type="text" name="conteudo" id="titulo"><?php echo $artigo['conteudo'] ?></textarea>
             </p>
             <p>
                 <input type="hidden" name="id" value="" />
