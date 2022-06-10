dc24eb2dbe12a276fc63d3890a5b1e4749937add
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 10:00:57 -0300

    inicio do curso de PDO

---
+++ pdo/src/Domain/Model/Student.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Alura\Pdo\Domain\Model;
+
+class Student
+{
+    private ?int $id;
+    private string $name;
+    private \DateTimeInterface $birthDate;
+
+    public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
+    {
+        $this->id = $id;
+        $this->name = $name;
+        $this->birthDate = $birthDate;
+    }
+
+    public function id(): ?int
+    {
+        return $this->id;
+    }
+
+    public function name(): string
+    {
+        return $this->name;
+    }
+
+    public function birthDate(): \DateTimeInterface
+    {
+        return $this->birthDate;
+    }
+
+    public function age(): int
+    {
+        return $this->birthDate
+            ->diff(new \DateTimeImmutable())
+            ->y;
+    }
+}
