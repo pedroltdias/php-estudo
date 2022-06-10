ed08f7f9847d9b07bb5ad9d5e02d08616ae7d2da
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 11:12:32 -0300

    busca de dados no bd

--- pdo/lista-alunos.php
+++ pdo/lista-alunos.php
@@ -1,9 +1,22 @@
 <?php
 
+use Alura\Pdo\Domain\Model\Student;
+
 require_once 'vendor/autoload.php';
 
 $databasePath = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $databasePath);
 
 $statement = $pdo->query('SELECT * FROM students;');
-$studentList = $statement->fetchAll();
+$studentDataList = $statement->fetchAll(PDO::FETCH_ASSOC);
+$studentList = [];
+
+foreach ($studentDataList as $studentData) {
+    $studentList = new Student(
+        $studentData['id'], 
+        $studentData['name'], 
+        new \DateTimeImmutable($studentData['birth_date'])
+    );
+}
+
+var_dump($studentList);
