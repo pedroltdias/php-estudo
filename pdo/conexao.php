70d842a671bbcd27e7669da1ce88c9311bcd21cc
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-10 10:32:25 -0300

    criacao da tabela 'students' no bd

--- pdo/conexao.php
+++ pdo/conexao.php
@@ -4,3 +4,5 @@ $caminhoBanco = __DIR__ . '/banco.sqlite';
 $pdo = new PDO('sqlite:' . $caminhoBanco);
 
 echo 'Conectei';
+
+$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
