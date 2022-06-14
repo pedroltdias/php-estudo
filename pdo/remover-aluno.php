0db892d55fbc2e53fbce290d25742c5cf4d7f68e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-14 13:47:55 -0300

    exclusao de alunos por id

--- pdo/remover-aluno.php
+++ pdo/remover-aluno.php
@@ -1,2 +1,13 @@
 <?php
 
+require_once 'vendor/autoload.php';
+
+$databasePath = __DIR__ . '/banco.sqlite';
+$pdo = new PDO('sqlite:' . $databasePath);
+
+$preparedStatement = $pdo->prepare('DELETE FROM students WHERE id = ?;');
+$preparedStatement-> bindValue(1, 2, PDO::PARAM_INT);   //Definindo que o parametro é INT e não STRING
+var_dump($preparedStatement->execute());
+
+$preparedStatement-> bindValue(1, 5, PDO::PARAM_INT);
+var_dump($preparedStatement->execute());
