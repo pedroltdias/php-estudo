c6a5d6876c651313e09b8a674f217b95ca81d3dc
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-14 13:39:29 -0300

    utilizando bindParam para salvar valores por referência na consulta

--- pdo/inserir-aluno.php
+++ pdo/inserir-aluno.php
@@ -9,13 +9,15 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 $student = new Student(
     null,
-    'Vinicius Dias',
-    new DateTimeImmutable('1997-10-15')
+    'Dias',
+    new \DateTimeImmutable('1998-10-15')
 );
+
+$name = $student->name();
 
 $sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(':name', $student->name());
+$statement->bindParam(':name', $student->name());
 $statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
