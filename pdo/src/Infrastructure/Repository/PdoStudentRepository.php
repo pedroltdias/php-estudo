cbfe542dd0458fea052913996e5d9c0809ae7b2c
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-14 14:42:43 -0300

    esqueleto da classe PdoStudentRepository

--- pdo/src/Infrastructure/Repository/PdoStudentRepository.php
+++ pdo/src/Infrastructure/Repository/PdoStudentRepository.php
@@ -2,7 +2,38 @@
 
 namespace Alura\Pdo\Infrastructure\Repository;
 
-class PdoStudentRepository
+use Alura\Pdo\Domain\Repository\Student;
+use Alura\Pdo\Domain\Repository\StudentRepository;
+use Alura\Pdo\Infrastructure\Persistence\ConnectionCreator;
+use DateTimeInterface;
+
+class PdoStudentRepository implements StudentRepository
 {
-    
+    private \PDO $connection;
+
+    public function __construct() 
+    {
+        $this->connection = ConnectionCreator::createConnection();
+    }
+
+    public function allStudents(): array
+    {
+        return;
+    }
+
+    public function studentsBirthAt(DateTimeInterface $birthDate): array
+    {
+        
+    }
+
+    public function save(Student $student): bool
+    {
+        
+    }
+
+    public function remove(Student $student): bool
+    {
+        
+    }
+
 }
