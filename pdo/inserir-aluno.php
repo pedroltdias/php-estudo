9d6efc428f41aa9590672b2fd709aeb1e16b2f59
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-14 13:24:28 -0300

    passagem de parametro utilizando parametro nomeado

--- pdo/inserir-aluno.php
+++ pdo/inserir-aluno.php
@@ -13,10 +13,10 @@ $student = new Student(
     new DateTimeImmutable('1997-10-15')
 );
 
-$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (?, ?);";
+$sqlInsert = "INSERT INTO students (name, birth_date) VALUES (:name, :birth_date);";
 $statement = $pdo->prepare($sqlInsert);
-$statement->bindValue(1, $student->name());
-$statement->bindValue(2, $student->birthDate()->format('Y-m-d'));
+$statement->bindValue(':name', $student->name());
+$statement->bindValue(':birth_date', $student->birthDate()->format('Y-m-d'));
 
 if ($statement->execute()) {
     echo "Aluno incluído!";
