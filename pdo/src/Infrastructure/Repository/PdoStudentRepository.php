69467cd0812c1cf6ff989a00c0f197f931797ea9
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 13:45:31 -0300

    metodo 'fillPhonesOf' removido

--- pdo/src/Infrastructure/Repository/PdoStudentRepository.php
+++ pdo/src/Infrastructure/Repository/PdoStudentRepository.php
@@ -41,37 +41,14 @@ class PdoStudentRepository implements StudentRepository
         $studentList = [];
 
         foreach ($studentDataList as $studentData) {
-            $student = new Student(
+            $studentList[] = new Student(
                 $studentData['id'],
                 $studentData['name'],
                 new \DateTimeImmutable($studentData['birth_date'])
             );
-
-            $this->fillPhonesOf($student);
-
-            $studentList[] = $student;
         }
 
         return $studentList;
-    }
-
-    private function fillPhonesOf (Student $student): void
-    {
-        $sqlQuery = 'SELECT id, area_code, number FROM phones WHERE student_id = ?';
-        $stmt = $this->connection->prepare($sqlQuery);
-        $stmt->bindValue(1, $student->id(), PDO::PARAM_INT);
-        $stmt->execute();
-
-        $phoneDataList = $stmt->fetchAll();
-        foreach ($phoneDataList as $phoneData) {
-            $phone = new Phone(
-                $phoneData['id'],
-                $phoneData['area_code'],
-                $phoneData['number']
-            );
-
-            $student->addPhone($phone);
-        }
     }
 
     public function save(Student $student): bool
