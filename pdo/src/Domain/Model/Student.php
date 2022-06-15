8ac44c84d5593e163442d750f645d6a56e395e7d
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 13:20:29 -0300

    atributo phone e seus metodos adicionados a model 'Student'

--- pdo/src/Domain/Model/Student.php
+++ pdo/src/Domain/Model/Student.php
@@ -7,6 +7,8 @@ class Student
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /** @var Phone[] */
+    private array $phones;
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +51,15 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phone[] = $phone;
+    }
+
+    public function phones(): array
+    {
+        return $this->phones();
     }
 }
