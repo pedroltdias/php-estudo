eb98f770f2eb31d36d75169999dda01786e5bcb8
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 15:23:05 -0300

    teste da conexao com o bd

---
+++ doctrine/teste.php
@@ -0,0 +1,10 @@
+<?php
+
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once 'vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+var_dump($entityManager->getConnection());
