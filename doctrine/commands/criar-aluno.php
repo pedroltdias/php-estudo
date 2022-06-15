4270167b5f91a43e8d06b081fed5ccae78680a18
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 16:18:53 -0300

    recebe o valor do nome da entidade pela linha de comando

--- doctrine/commands/criar-aluno.php
+++ doctrine/commands/criar-aluno.php
@@ -6,12 +6,11 @@ use Alura\Doctrine\Helper\EntityManagerFactory;
 require_once 'vendor/autoload.php';
 
 $aluno = new Aluno();
-$aluno->setNome('Pedroca Dias');
+$aluno->setNome($argv[1]);
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
 
 $entityManager->persist($aluno);
-$aluno->setNome('Pedro Dias');
 
 $entityManager->flush();
