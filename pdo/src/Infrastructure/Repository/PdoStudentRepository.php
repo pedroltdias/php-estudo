d689f4cb8b93630df33a48f0800e6db03569e9f2
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 13:33:05 -0300

    metodo 'fillPhonesOf'

--- pdo/src/Infrastructure/Repository/PdoStudentRepository.php
+++ pdo/src/Infrastructure/Repository/PdoStudentRepository.php
@@ -2,6 +2,7 @@
 
 namespace Alura\Pdo\Infrastructure\Repository;
 
+use Alura\Pdo\Domain\Model\Phone;
 use Alura\Pdo\Domain\Model\Student;
 use Alura\Pdo\Domain\Repository\StudentRepository;
 use PDO;
@@ -52,6 +53,25 @@ class PdoStudentRepository implements StudentRepository
         }
 
         return $studentList;
+    }
+
+    public function fillPhonesOf (Student $student): void
+    {
+        $sqlQuery = 'SELECT id, area_code, number FROM phones WHERE student_id = ?';
+        $stmt = $this->connection->prepare($sqlQuery);
+        $stmt->bindValue(1, $student->id(), PDO::PARAM_INT);
+        $stmt->execute();
+
+        $phoneDataList = $stmt->fetchAll();
+        foreach ($phoneDataList as $phoneData) {
+            $phone = new Phone(
+                $phoneData['id'],
+                $phoneData['area_code'],
+                $phoneData['number']
+            );
+
+            $student->addPhone($phone);
+        }
     }
 
     public function save(Student $student): bool
