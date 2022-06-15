d9ebb7b79190080f47eb68d25f2a43e0c46d1c26
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 15:33:18 -0300

    getters e setters

--- doctrine/src/Entity/Aluno.php
+++ doctrine/src/Entity/Aluno.php
@@ -4,5 +4,21 @@ namespace Alura\Doctrine\Entity;
 
 class Aluno
 {
-    
+    private int $id;
+    private string $nome;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome($nome): void
+    {
+        $this->nome = $nome;
+    }
 }
