c0434f89b15289b37456b06401731467da1e9135
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 13:13:20 -0300

    SQL para criar as tabelas phones e students

--- pdo/conexao.php
+++ pdo/conexao.php
@@ -5,4 +5,20 @@ $pdo = new PDO('sqlite:' . $databasePath);
 
 echo 'Conectei';
 
-$pdo->exec('CREATE TABLE students (id INTEGER PRIMARY KEY, name TEXT, birth_date TEXT);');
+$createTableSql = '
+    CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        birth_date TEXT
+    );
+
+    CREATE TABLE IF NOT EXISTS phones (
+        id INTEGER PRIMARY KEY,
+        area_code TEXT,
+        number TEXT,
+        student_id INTEGER,
+        FOREIGN KEY (student_id) REFERENCES students(id)
+    );
+';
+
+$pdo->exec($createTableSql);
