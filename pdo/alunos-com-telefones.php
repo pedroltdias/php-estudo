81181e33e51e5c24dd51027f174fc09878d31267
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 14:17:20 -0300

    linha de codigo para ser revisada - fatal error

--- pdo/alunos-com-telefones.php
+++ pdo/alunos-com-telefones.php
@@ -11,4 +11,5 @@ $repository = new PdoStudentRepository($connection);
 /** @var \Alura\Pdo\Domain\Model\Student[] $studentList */
 $studentList = $repository->studentsWithPhones();
 
+// echo $studentList[1]->phones()[0]->formattedPhone();
 var_dump($studentList);
