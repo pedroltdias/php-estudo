97bc51214c038dc53001cf88d71c459373d3057a
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 16:38:23 -0300

    atualizacao do nome de um usuario sem usar repositorio

--- doctrine/commands/atualizar-nome-aluno.php
+++ doctrine/commands/atualizar-nome-aluno.php
@@ -8,12 +8,10 @@ require_once 'vendor/autoload.php';
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
 
-$alunoRepository = $entityManager->getRepository(Aluno::class);
-
 $id = $argv[1];
 $novoNome = $argv[2];
 
-$aluno = $alunoRepository->find($id);
+$aluno = $entityManager->find(Aluno::class, $id);
 $aluno->setNome($novoNome);
 
 $entityManager->flush();
