12e77a30cec7324045572389e8785d54b61e953f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-15 16:30:14 -0300

    metodos para busca de alunos usando doctrine

--- doctrine/commands/buscar-alunos.php
+++ doctrine/commands/buscar-alunos.php
@@ -1,8 +1,30 @@
 <?php
 
+use Alura\Doctrine\Entity\Aluno;
 use Alura\Doctrine\Helper\EntityManagerFactory;
 
 require_once 'vendor/autoload.php';
 
 $entityManagerFactory = new EntityManagerFactory();
 $entityManager = $entityManagerFactory->getEntityManager();
+
+$alunoRepository = $entityManager->getRepository(Aluno::class);
+
+$alunoList = $alunoRepository->findAll();
+
+foreach ($alunoList as $aluno) {
+    echo "[{$aluno->getId()}]: {$aluno->getNome()}" . PHP_EOL;
+}
+
+$paulo = $alunoRepository->find(3);
+echo $paulo->getNome();
+
+$mauricio = $alunoRepository->findBy([
+    'nome' => 'Mauricio Dias'
+]);
+var_dump($mauricio);
+
+$madalena = $alunoRepository->findOneBy([
+    'nome' => 'Madalena Dias'
+]);
+var_dump($madalena);
