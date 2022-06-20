22abde53e11943c1d3f7eb21c517d120d6485274
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-20 15:10:20 -0300

    nova entidade 'Curso'

---
+++ doctrine/src/Entity/Curso.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Alura\Doctrine\Entity;
+
+class Curso
+{
+    private $id;
+    private $nome;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getNome(): string
+    {
+        return $this->nome;
+    }
+
+    public function setNome(string $nome)
+    {
+        $this->nome = $nome;
+    }
+}
