564b303dd922f16d7df90c86321c031a2c909596
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-20 16:21:09 -0300

    usando DQL para buscar alunos

---
+++ doctrine/commands/dql-buscar-alunos.php
@@ -0,0 +1,25 @@
+<?php
+
+use Alura\Doctrine\Entity\Aluno;
+use Alura\Doctrine\Entity\Telefone;
+use Alura\Doctrine\Helper\EntityManagerFactory;
+
+require_once 'vendor/autoload.php';
+
+$entityManagerFactory = new EntityManagerFactory();
+$entityManager = $entityManagerFactory->getEntityManager();
+
+$dql = 'SELECT aluno FROM Alura\\Doctrine\Entity\Aluno aluno';
+$query = $entityManager->createQuery($dql);
+$alunoList = $query->getResult();
+
+foreach ($alunoList as $aluno) {
+    $telefones = $aluno
+        ->getTelefones()
+        ->map(function (Telefone $telefone) {
+            return $telefone->getNumero();
+        })
+        ->toArray();
+    echo "[{$aluno->getId()}]: {$aluno->getNome()}" . PHP_EOL;
+    echo "Telefones: " . implode(', ', $telefones) . PHP_EOL;
+}
