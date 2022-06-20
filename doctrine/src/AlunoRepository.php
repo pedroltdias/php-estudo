f8bede9e8883769ba330d4b038c2adb5eef9c35d
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-20 16:54:00 -0300

    criacao do esqueleto 'Aluno Repository'

---
+++ doctrine/src/AlunoRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Alura\Doctrine\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+class AlunoRepository extends EntityRepository
+{
+    public function buscaCursosPorAluno()
+    {
+
+    }
+}
