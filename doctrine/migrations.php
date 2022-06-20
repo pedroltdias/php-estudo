a741679b5d30f7e53e2bf5cc4b7b71f798dbed00
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-20 14:00:25 -0300

    arquivo de configuracao das migrations

---
+++ doctrine/migrations.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'name' => 'My Project Migrations',
+    'migrations_namespace' => 'MyProject\Migrations',
+    'table_name' => 'doctrine_migration_versions',
+    'column_name' => 'version',
+    'column_length' => 14,
+    'executed_at_column_name' => 'executed_at',
+    'migrations_directory' => '/data/doctrine/migrations-docs-example/lib/MyProject/Migrations',
+    'all_or_nothing' => true,
+];
