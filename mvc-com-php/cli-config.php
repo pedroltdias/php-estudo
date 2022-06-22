4de8ee34e9a5ac460c7fe20827d695aa38af7065
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-22 12:46:30 -0300

    adicionando libs faltantes

--- mvc-com-php/cli-config.php
+++ mvc-com-php/cli-config.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once '/vendor/autoload.php';
 
 return \Doctrine\ORM\Tools\Console\ConsoleRunner::createHelperSet(
     (new \Alura\Cursos\Infra\EntitymanagerCreator())->getEntityManager()
