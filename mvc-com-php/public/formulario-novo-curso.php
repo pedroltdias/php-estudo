9052432aa427982b0b149e8687748b2b4de76488
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-22 16:18:10 -0300

    criacao do arquivo para novo curso

---
+++ mvc-com-php/public/formulario-novo-curso.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Listar cursos</h1>
+        </div>
+
+        <a href="formulario-novo-curso.php" class="btn btn-primary mb-2">Novo Curso</a>
+
+        <ul class="list-group">
+            <?php foreach ($cursos as $curso) : ?>
+                <li class="list-group-item">
+                    <?= $curso->getDescricao(); ?>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </div>
+</body>
+
+</html>
