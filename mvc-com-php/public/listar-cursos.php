39ef2163c79e851cbc313b11b4ebe721fc2fce42
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-22 16:17:27 -0300

    criacao do botao para novo curso

--- mvc-com-php/public/listar-cursos.php
+++ mvc-com-php/public/listar-cursos.php
@@ -19,6 +19,9 @@ $cursos = $repositorioDeCursos->findAll();
         <div class="jumbotron">
             <h1>Listar cursos</h1>
         </div>
+
+        <a href="formulario-novo-curso.php" class="btn btn-primary mb-2">Novo Curso</a>
+
         <ul class="list-group">
             <?php foreach ($cursos as $curso) : ?>
                 <li class="list-group-item">
