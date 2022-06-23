19a839ea7e96f4422070038af39904ead98e1d67
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-23 16:42:41 -0300

    pagina de visualizacao de criacao de novo curso

---
+++ mvc-com-php/view/cursos/formulario.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Criar Novo Curso</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1>Novo Curso</h1>
+        </div>
+
+        <form action="">
+            <div class="form-group">
+                <label for="descricao">Descrição</label>
+                <input type="text" id="descricao" name="descricao" class="form-control">
+            </div>
+            <button class="btn btn-primary">Salvar</button>
+        </form>
+
+    </div>
+</body>
+
+</html>
