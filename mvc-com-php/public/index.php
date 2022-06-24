3b4efa7b334f57a34727ba034fbd5c492f536cc5
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-24 08:39:59 -0300

    reformulação da index.php utilizando rotas

--- mvc-com-php/public/index.php
+++ mvc-com-php/public/index.php
@@ -3,25 +3,18 @@
 require __DIR__ . '/../vendor/autoload.php';
 
 use Alura\Cursos\Controller\FormularioInsercao;
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
 
+$caminho = $_SERVER['PATH_INFO'];
 $rotas = require __DIR__ . '/../config/routes.php';
 
-switch ($_SERVER['PATH_INFO']) {
-    case '/listar-cursos':
-        $controlador = new ListarCursos();
-        $controlador->processaRequisicao();
-        break;
-    case '/formulario-novo-curso':
-        $controlador = new FormularioInsercao();
-        $controlador->processaRequisicao();
-        break;
-    case '/salvar-curso':
-        $controlador = new Persistencia();
-        $controlador->processaRequisicao();
-        break;
-    default:
-        echo "ERROR 404";
-        break;
+if (!array_key_exists($caminho, $rotas)) {
+    echo "Erro 404";
+    exit();
 }
+
+$classeControladora = $rotas[$caminho];
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
