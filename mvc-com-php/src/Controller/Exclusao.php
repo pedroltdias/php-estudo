fc91418cc23e7b899c1fed43f84af7abb291fdde
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-27 16:42:10 -0300

    implementacao da controller de exclusoes

--- mvc-com-php/src/Controller/Exclusao.php
+++ mvc-com-php/src/Controller/Exclusao.php
@@ -4,5 +4,17 @@ namespace Alura\Cursos\Controller;
 
 class Exclusao implements InterfaceControladorRequisicao
 {
-    
+    public function processaRequisicao(): void
+    {
+        $id = filter_input(
+            INPUT_GET, 
+            'id', 
+            FILTER_VALIDATE_INT
+        );
+
+        if ($id === false) {
+            header('Location: /listar-cursos');
+        }
+    }
+
 }
