2c2e3570dfbaeb0a9cf5f1b21c962ecdadfe791e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-27 16:42:48 -0300

    implementacao da rota de exclusao

--- mvc-com-php/config/routes.php
+++ mvc-com-php/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioInsercao;
 use Alura\Cursos\Controller\ListarCursos;
 use Alura\Cursos\Controller\Persistencia;
@@ -7,5 +8,6 @@ use Alura\Cursos\Controller\Persistencia;
 return [
     '/listar-cursos' => ListarCursos::class,
     '/novo-curso' => FormularioInsercao::class,
-    '/salvar-curso' => Persistencia::class
+    '/salvar-curso' => Persistencia::class,
+    '/excluir-curso' => Exclusao::class
 ];
