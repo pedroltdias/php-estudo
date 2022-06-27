fb5dbdc3be4ad94fb64700a044864431196e6390
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-27 17:03:16 -0300

    erro ao requirir o autoload consertado

--- mvc-com-php/view/cursos/listar-cursos.php
+++ mvc-com-php/view/cursos/listar-cursos.php
@@ -6,7 +6,7 @@
     <?php foreach ($cursos as $curso) : ?>
         <li class="list-group-item d-flex justify-content-between">
             <?= $curso->getDescricao(); ?>
-            <a href="/excluir-curso?id=<?php $curso->getId(); ?>" class="btn btn-danger btn-sm">Excluir</a>
+            <a href="/excluir-curso?id=<?= $curso->getId(); ?>" class="btn btn-danger btn-sm">Excluir</a>
         </li>
     <?php endforeach; ?>
 </ul>
