6a7ce11bad37014a1601d5e63ebaecd8f81471d2
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-27 16:47:31 -0300

    criacao do entityManager e implementacao do codigo de exclusao

--- mvc-com-php/src/Controller/Exclusao.php
+++ mvc-com-php/src/Controller/Exclusao.php
@@ -2,8 +2,19 @@
 
 namespace Alura\Cursos\Controller;
 
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
 class Exclusao implements InterfaceControladorRequisicao
 {
+    private $entityManager;
+
+    public function __construct() 
+    {
+        $this->entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+    }
+
     public function processaRequisicao(): void
     {
         $id = filter_input(
@@ -15,6 +26,10 @@ class Exclusao implements InterfaceControladorRequisicao
         if (is_null($id) || $id === false) {
             header('Location: /listar-cursos');
         }
+
+        $curso = $this->entityManager->getReference(Curso::class, $id);
+        $this->entityManager->remove($curso);
+        $this->entityManager->flush();
     }
 
 }
