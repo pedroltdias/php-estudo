b1aaf8a12a06a5fddbc986beae71233211e9cdcf
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-28 16:41:32 -0300

    achando curso pelo id

--- mvc-com-php/src/Controller/FormularioEdicao.php
+++ mvc-com-php/src/Controller/FormularioEdicao.php
@@ -2,8 +2,20 @@
 
 namespace Alura\Cursos\Controller;
 
+use Alura\Cursos\Entity\Curso;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
 class Formulario implements InterfaceControladorRequisicao
 {
+    private $repositorioCursos;
+
+    public function __construct() 
+    {
+        $entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+        $this->repositorioCursos = $entityManager
+            ->getRepository(Curso::class);
+    }
 
     public function processaRequisicao(): void
     {
@@ -17,6 +29,9 @@ class Formulario implements InterfaceControladorRequisicao
             header('Location: /listar-cursos');
             return;
         }
+
+        $curso = $this->repositorioCursos->find($id);
+        require __DIR__ . '/../../view/cursos/formulario.php';
 
     }
 }
