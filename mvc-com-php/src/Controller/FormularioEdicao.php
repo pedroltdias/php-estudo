5e3888700c5cfefa1d10930117039eeeafbca965
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-28 16:38:45 -0300

    pegando o id do curso a se alterar

--- mvc-com-php/src/Controller/FormularioEdicao.php
+++ mvc-com-php/src/Controller/FormularioEdicao.php
@@ -1,1 +1,22 @@
 <?php
+
+namespace Alura\Cursos\Controller;
+
+class Formulario implements InterfaceControladorRequisicao
+{
+
+    public function processaRequisicao(): void
+    {
+        $id = filter_input(
+                INPUT_GET,
+                'id',
+                FILTER_VALIDATE_INT
+        );
+
+        if (is_null($id) || $id === false) {
+            header('Location: /listar-cursos');
+            return;
+        }
+
+    }
+}
