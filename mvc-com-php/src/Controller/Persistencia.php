2d8de44e76be902d66fa1ceec90c550e6921390f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-29 13:49:38 -0300

    validacao para efetuar alteracao no bd

--- mvc-com-php/src/Controller/Persistencia.php
+++ mvc-com-php/src/Controller/Persistencia.php
@@ -21,11 +21,24 @@ class Persistencia implements InterfaceControladorRequisicao
             'descricao',
             FILTER_SANITIZE_FULL_SPECIAL_CHARS
         );
-
+        
         $curso = new Curso();
         $curso->setDescricao($descricao);
-        $this->entityManager->persist($curso);
-        $this->entityManager->flush();
+
+        $id = filter_input(
+            INPUT_GET,
+            'id',
+            FILTER_VALIDATE_INT
+        );
+
+        if (!is_null($id) || $id !== false) {
+            $curso->setId($id);
+            $this->entityManager->merge($curso);
+        } else {
+            $this->entityManager->persist($curso);
+            $this->entityManager->flush();
+        }
+
 
         header('Location: /listar-cursos', true, 302);
     }
