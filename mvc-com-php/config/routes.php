1d0969ce6c0f363e65d251f524f0f094b0fa3f1c
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-29 14:52:40 -0300

    C:/Users/pedro.dias/AppData/Local/Programs/Git/login adicionado as rotas

--- mvc-com-php/config/routes.php
+++ mvc-com-php/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 
-use Alura\Cursos\Controller\{Exclusao, FormularioEdicao, FormularioInsercao, ListarCursos, Persistencia};
+use Alura\Cursos\Controller\{Exclusao, FormularioEdicao, FormularioInsercao, FormularioLogin, ListarCursos, Persistencia};
 
 return [
     '/listar-cursos' => ListarCursos::class,
@@ -8,4 +8,5 @@ return [
     '/salvar-curso' => Persistencia::class,
     '/excluir-curso' => Exclusao::class,
     '/alterar-curso' => FormularioEdicao::class,
+    '/login' => FormularioLogin::class
 ];
