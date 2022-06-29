eb68621b0113ec59581d4d4b9f4371cb4223a099
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-29 15:56:28 -0300

    validacao de email e senha de usuario

--- mvc-com-php/src/Controller/RealizarLogin.php
+++ mvc-com-php/src/Controller/RealizarLogin.php
@@ -2,8 +2,24 @@
 
 namespace Alura\Cursos\Controller;
 
+use Alura\Armazenamento\Entity\Usuario;
+use Alura\Cursos\Infra\EntityManagerCreator;
+
 class RealizarLogin implements InterfaceControladorRequisicao
 {
+    /**
+     * @var \Doctrine\Common\Persistence\ObjectRepository
+     */
+    private $repositorioDeUsuarios;
+
+    public function __construct()
+    {
+        $entityManager = (new EntityManagerCreator())
+            ->getEntityManager();
+        $this->repositorioDeUsuarios = $entityManager
+            ->getRepository(Usuario::class);
+    }
+
     public function processaRequisicao(): void
     {
         $email = filter_input(
@@ -13,10 +29,24 @@ class RealizarLogin implements InterfaceControladorRequisicao
         );
 
         if (is_null($email) || $email === false) {
-            echo "E-mail inválido";
+            echo "E-mail digitado é inválido!";
             return;
         }
 
-        
+        $senha = filter_input(
+            INPUT_POST,
+            'senha',
+            FILTER_SANITIZE_STRING
+        );
+
+        $usuario = $this->repositorioDeUsuarios
+            ->findOneBy(['email' => $email]);
+
+        if (is_null($usuario) || !$usuario->senhaEstaCorreta($senha)) {
+            echo "E-mail ou senha inválidos.";
+            return;
+        }
+
+        header('Location: /listar-curso');
     }
 }
