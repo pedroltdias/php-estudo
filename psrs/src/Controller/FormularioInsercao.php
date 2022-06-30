87b585a3bed50a31ea836aa9e9bce746e4ebc86f
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-30 15:26:14 -0300

    criacao da controller FormularioInsercao e require da nyholm/psr7

---
+++ psrs/src/Controller/FormularioInsercao.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Alura\Cursos\Controller;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class FormularioInsercao implements InterfaceControladorRequisicao
+{
+    public function processaRequisicao(ServerRequestInterface $request): ResponseInterface
+    {
+        
+    }
+}
