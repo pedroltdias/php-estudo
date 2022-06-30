1f22f7e555951cc4bc5f245511555ac8ab7d6db6
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-30 15:12:02 -0300

    importacao da public/index.php

---
+++ psrs/public/index.php
@@ -0,0 +1,27 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Alura\Cursos\Controller\InterfaceControladorRequisicao;
+
+$caminho = $_SERVER['PATH_INFO'];
+$rotas = require __DIR__ . '/../config/routes.php';
+
+if (!array_key_exists($caminho, $rotas)) {
+    http_response_code(404);
+    exit();
+}
+
+session_start();
+
+$ehRotaDeLogin = str_contains($caminho, 'login');
+
+if (!isset($_SESSION['logado']) && !$ehRotaDeLogin) {
+    header('Location: /login');
+    exit();
+}
+
+$classeControladora = $rotas[$caminho];
+/** @var InterfaceControladorRequisicao $controlador */
+$controlador = new $classeControladora();
+$controlador->processaRequisicao();
