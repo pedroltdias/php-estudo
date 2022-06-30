7bed757bcf6782b005224cb5d33802c1b6364bf3
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-30 14:19:10 -0300

    feedback para o usuario ao excluir curso

--- mvc-com-php/src/Controller/Exclusao.php
+++ mvc-com-php/src/Controller/Exclusao.php
@@ -9,7 +9,7 @@ class Exclusao implements InterfaceControladorRequisicao
 {
     private $entityManager;
 
-    public function __construct() 
+    public function __construct()
     {
         $this->entityManager = (new EntityManagerCreator())
             ->getEntityManager();
@@ -18,12 +18,14 @@ class Exclusao implements InterfaceControladorRequisicao
     public function processaRequisicao(): void
     {
         $id = filter_input(
-            INPUT_GET, 
-            'id', 
+            INPUT_GET,
+            'id',
             FILTER_VALIDATE_INT
         );
 
         if (is_null($id) || $id === false) {
+            $_SESSION['mensagem'] = "Curso inexistente!";
+            $_SESSION['tipo_mensagem'] = 'danger';
             header('Location: /listar-cursos');
             return;
         }
@@ -31,7 +33,10 @@ class Exclusao implements InterfaceControladorRequisicao
         $curso = $this->entityManager->getReference(Curso::class, $id);
         $this->entityManager->remove($curso);
         $this->entityManager->flush();
+
+        $_SESSION['mensagem'] = "Curso removido com sucesso!";
+        $_SESSION['tipo_mensagem'] = 'success';
+
         header('Location: /listar-cursos');
     }
-
 }
