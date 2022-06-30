0e89e639ef1f92fa6e744fe3d248cd095645b01a
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-06-30 13:30:19 -0300

    adicionando uma navbar

--- mvc-com-php/view/inicio-html.php
+++ mvc-com-php/view/inicio-html.php
@@ -8,6 +8,18 @@
 </head>
 
 <body>
+
+    <nav class="navbar navbar-dark bg-dark">
+        <a class="navbar-brand" href="/listar-cursos">Home</a>
+
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="/logout">Sair</a>
+            </li>
+        </ul>
+
+    </nav>
+
     <div class="container">
         <div class="jumbotron">
             <h1><?php echo $titulo; ?></h1>
