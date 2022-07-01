bfb545483cab58dbe8ea5787866b814aa568c7b6
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-01 11:12:36 -0300

    criando a controller de exclusao utilizando requesthandler

--- psrs/config/routes.php
+++ psrs/config/routes.php
@@ -1,7 +1,9 @@
 <?php
 
+use Alura\Cursos\Controller\Exclusao;
 use Alura\Cursos\Controller\FormularioInsercao;
 
 return [
     '/novo-curso' => FormularioInsercao::class,
+    '/excluir-curso' => Exclusao::class,
 ];
