1ad0474031862ef9a8f73454308ab0c1c878d7ab
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 13:08:17 -0300

    ajustando as rotas para /series

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\SeriesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/series', function () {
-    echo "Hello World!";
-});
+Route::get('/series',[SeriesController::class, 'listarSeries']);
