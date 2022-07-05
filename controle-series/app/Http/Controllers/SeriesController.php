b6e9071715c0b5dc40bcf13851c765609e50d799
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 13:58:04 -0300

    retornando com o metodo ->with

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -14,6 +14,6 @@ class SeriesController extends Controller
             "Grey's Anatomy"
         ];
 
-        return view('listar-series', compact('series'));
+        return view('listar-series')->with('series', $series);
     }
 }
