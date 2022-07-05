0296f2235acac027f2fc0d4d4f33f4484765518d
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 15:57:00 -0300

    utilizacao do Eloquent ORM para fazer as funcoes no banco de dados

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Serie;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -9,7 +10,8 @@ class SeriesController extends Controller
 {
     public function index()
     {
-        $series = DB::select('SELECT nome FROM series;');
+//        $series = Serie::all();
+        $series = Serie::query()->orderBy('nome')->get(); //Series em ordem alfabetica
 
         return view('series.index')->with('series', $series);
     }
@@ -22,9 +24,10 @@ class SeriesController extends Controller
     public function store(Request $request)
     {
         $nomeSerie = $request->input('nome');
+        $serie = new Serie();
+        $serie->nome = $nomeSerie;
+        $serie->save();
 
-        if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
-            return redirect('/series');
-        }
+        return redirect('/series');
     }
 }
