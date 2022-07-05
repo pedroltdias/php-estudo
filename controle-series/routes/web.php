544fcb0924f4c298bfc8c94bf191da1c5dbe2165
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 17:19:15 -0300

    utilizando o nomeamento padrao do laravel para criar as rotas de um controller

--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -18,8 +18,4 @@ Route::get('/', function () {
     return redirect('/series');
 });
 
-Route::controller(SeriesController::class)->group(function () {
-    Route::get('/series', 'index')->name('series.index');
-    Route::get('/series/criar', 'create')->name('series.create');
-    Route::post('/series/salvar',  'store')->name('series.store');
-});
+Route::resource('/series', SeriesController::class);
