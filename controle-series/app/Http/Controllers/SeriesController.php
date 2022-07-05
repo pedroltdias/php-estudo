5022ad9f8749718018f3cb808a9aa75eba707138
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 13:43:09 -0300

    retornando variavel series utilizando a funcao 'compact'

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -14,8 +14,6 @@ class SeriesController extends Controller
             "Grey's Anatomy"
         ];
 
-        return view('listar-series', [
-            'series' => $series
-        ]);
+        return view('listar-series', compact('series'));
     }
 }
