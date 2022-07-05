205c85a65cc4cf46421aac869ec5098f47565b93
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 14:05:36 -0300

    utilizando um layout geral para todas as spaginas e importando na series/index

--- controle-series/resources/views/series/index.blade.php
+++ controle-series/resources/views/series/index.blade.php
@@ -1,21 +1,7 @@
-<!doctype html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Series</title>
-</head>
-<body>
-
-<h1>Séries</h1>
-
-<ul>
-    @foreach ($series as $serie)
-    <li>{{ $serie }}</li>
-    @endforeach
-</ul>
-
-</body>
-</html>
+<x-layout title="Séries">
+    <ul>
+        @foreach ($series as $serie)
+        <li>{{ $serie }}</li>
+        @endforeach
+    </ul>
+</x-layout>
