497f3b4df4d8c1722bc8c2a3af3df3f0e6298454
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-05 15:46:46 -0300

    acessando corretamente o bd e exibindo na view

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -3,16 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index(Request $request)
+    public function index()
     {
-        $series = [
-            "Punisher",
-            "Lost",
-            "Grey's Anatomy"
-        ];
+        $series = DB::select('SELECT nome FROM series;');
+        dd($series);
 
         return view('series.index')->with('series', $series);
     }
@@ -20,5 +18,15 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
+    }
+
+    public function store(Request $request)
+    {
+        $nomeSerie = $request->input('nome');
+        if(DB::insert('INSERT INTO series (nome) VALUES (?)', [$nomeSerie])) {
+            return "ok";
+        } else {
+            return "deu erro";
+        }
     }
 }
