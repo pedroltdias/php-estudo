1a2c70e656936297b734375dbea311678302b69b
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-06 16:50:15 -0300

    redirecionamento e exibicao de erros

--- controle-series/resources/views/components/layout.blade.php
+++ controle-series/resources/views/components/layout.blade.php
@@ -14,6 +14,16 @@
 <div class="container">
     <h1>{{ $title }}</h1>
 
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
     {{ $slot  }}
 </div>
 
