ff63a93c5851cd0d220accde437f7c24c76df25b
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-06 16:37:26 -0300

    validando entrada de nova serie

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -26,6 +26,10 @@ class SeriesController extends Controller
 
     public function store(Request $request)
     {
+        $request->validate([
+            'nome' => ['required', 'min:3']
+        ]);
+
         $serie = Serie::create($request->all());
 
         return to_route('series.index')
