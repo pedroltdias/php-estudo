a20fd3af8e24b9bbb19d44ce6c94416ecb046e31
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-08 09:13:09 -0300

    sintaxe alternativa para fazer transacao

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -28,32 +28,33 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = null;
-        DB::transaction(function () use ($request, &$serie) {
-            $serie = Series::create($request->all());
-            $seasons = [];
+        DB::beginTransaction();
+        $serie = Series::create($request->all());
+        $seasons = [];
 
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i,
+        for ($i = 1; $i <= $request->seasonsQty; $i++) {
+            $seasons[] = [
+                'series_id' => $serie->id,
+                'number' => $i,
+            ];
+        }
+
+        Season::insert($seasons);
+
+        $episodes = [];
+        foreach ($serie->seasons as $season){
+            for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+                $episodes[] = [
+                    'season_id' => $season->id,
+                    'number' => $j
                 ];
             }
+        }
 
-            Season::insert($seasons);
+        Episode::insert($episodes);
+        DB::commit();
 
-            $episodes = [];
-            foreach ($serie->seasons as $season){
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }
-            }
-
-            Episode::insert($episodes);
-        });
+        DB::rollBack();
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
