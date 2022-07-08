3c421f36d47c2ed4794632691984d55ada257fc3
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-08 09:29:25 -0300

    criacao do metodo add, responsavel por adicionar novas series ao bd

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -28,33 +28,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = DB::transaction(function () use ($request) {
-            $serie = Series::create($request->all());
-            $seasons = [];
 
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i,
-                ];
-            }
-
-            Season::insert($seasons);
-
-            $episodes = [];
-            foreach ($serie->seasons as $season){
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }
-            }
-
-            Episode::insert($episodes);
-
-            return $serie;
-        });
 
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
