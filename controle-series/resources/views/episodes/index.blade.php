433b13fa44969b5a21be7d89c154fdb24edb9dbd
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-12 16:36:46 -0300

    rota para salvar episodio já assistido e a view para marcar os episodios

--- controle-series/resources/views/episodes/index.blade.php
+++ controle-series/resources/views/episodes/index.blade.php
@@ -1,11 +1,14 @@
 <x-layout title="Episódios">
-    <ul class="list-group">
-        @foreach ($episodes as $episode)
-            <li class="list-group-item d-flex justify-content-between align-items-center">
-                Episódio {{ $episode->number }}
-                <input type="checkbox" name="episodes[]" value="{{ $episode->id }}">
-            </li>
-        @endforeach
-    </ul>
-    <button class="btn btn-primary mt-2 mb-2">Salvar</button>
+    <form method="post">
+        @csrf
+        <ul class="list-group">
+            @foreach ($episodes as $episode)
+                <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Episódio {{ $episode->number }}
+                    <input type="checkbox" name="episodes[]" value="{{ $episode->id }}">
+                </li>
+            @endforeach
+        </ul>
+        <button class="btn btn-primary mt-2 mb-2">Salvar</button>
+    </form>
 </x-layout>
--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -26,3 +26,6 @@ Route::resource('/series', SeriesController::class)
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
 Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
+Route::post('/seasons/{season}/episodes', function (\Illuminate\Http\Request $request){
+    dd($request->all());
+});
