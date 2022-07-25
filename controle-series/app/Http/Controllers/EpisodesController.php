8897d80d14d88763639c5cdefdcc1d93285d3f33
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-25 15:21:39 -0300

    salvando episodios assistidos no banco de dados

--- controle-series/app/Http/Controllers/EpisodesController.php
+++ controle-series/app/Http/Controllers/EpisodesController.php
@@ -2,12 +2,25 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Episode;
 use App\Models\Season;
+use Illuminate\Http\Request;
 
 class EpisodesController
 {
     public function index(Season $season)
     {
         return view('episodes.index', ['episodes' => $season->episodes]);
+    }
+
+    public function update(Request $request, Season $season)
+    {
+        $watchedEpisodes = $request->episodes;
+        $season->episodes->each(function (Episode $episode) use ($watchedEpisodes) {
+            $episode->watched = in_array($episode->id, $watchedEpisodes);
+        });
+        $season->push();
+
+        return to_route('episodes.index', $season->id);
     }
 }
--- controle-series/routes/web.php
+++ controle-series/routes/web.php
@@ -26,6 +26,4 @@ Route::resource('/series', SeriesController::class)
 Route::get('/series/{series}/seasons', [SeasonsController::class, 'index'])->name('seasons.index');
 
 Route::get('/seasons/{season}/episodes', [EpisodesController::class, 'index'])->name('episodes.index');
-Route::post('/seasons/{season}/episodes', function (\Illuminate\Http\Request $request){
-    dd($request->all());
-});
+Route::post('/seasons/{season}/episodes', [EpisodesController::class, 'update'])->name('episodes.update');
