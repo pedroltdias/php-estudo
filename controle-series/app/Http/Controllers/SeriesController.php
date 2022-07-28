0756c853960e581746448d0958d119b767927a3b
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-28 14:49:27 -0300

    migration e adaptacoes ao codigo para adicionar imagem a series

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -34,6 +34,7 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
+        dd($request->file('cover'));
         $serie = $this->repository->add($request);
         \App\Events\SeriesCreated::dispatch(
             $serie->nome,
--- controle-series/app/Models/Series.php
+++ controle-series/app/Models/Series.php
@@ -9,7 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Series extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome'];
+    protected $fillable = ['nome', 'cover'];
     public $timestamps = false;
 
     public function seasons()
