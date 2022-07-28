2d554a2684cdaaf22c28cce503c12b6251dbe271
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-28 14:25:21 -0300

    criacao de um listener/evento para gerar logs ao adicionar serie

--- controle-series/app/Listeners/EmailUsersAboutSeriesCreated.php
+++ controle-series/app/Listeners/EmailUsersAboutSeriesCreated.php
@@ -9,7 +9,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class EmailUsersAboutSeriesCreated
+class EmailUsersAboutSeriesCreated implements ShouldQueue
 {
     /**
      * Create the event listener.
--- controle-series/app/Providers/EventServiceProvider.php
+++ controle-series/app/Providers/EventServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use App\Events\SeriesCreated;
 use App\Listeners\EmailUsersAboutSeriesCreated;
+use App\Listeners\LogSeriesCreated;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -22,6 +23,7 @@ class EventServiceProvider extends ServiceProvider
         ],
         SeriesCreated::class => [
             EmailUsersAboutSeriesCreated::class,
+            LogSeriesCreated::class,
         ],
     ];
 
