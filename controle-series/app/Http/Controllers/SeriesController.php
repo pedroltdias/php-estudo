ddc378e2a9af07f2bddb744d8d1258081f853265
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-28 13:34:37 -0300

    criacao de um event listener a fim de desacoplar o codigo

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -36,20 +36,6 @@ class SeriesController extends Controller
     {
         $serie = $this->repository->add($request);
 
-        $userList = User::all();
-
-        foreach ($userList as $index => $user) {
-            $email = new SeriesCreated(
-                $serie->nome,
-                $serie->id,
-                $request->seasonsQty,
-                $request->episodesPerSeason
-            );
-
-            $when = now()->addSeconds($index * 5);
-            Mail::to($user)->later($when, $email);
-        }
-
         return to_route('series.index')
             ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
     }
---
+++ controle-series/app/Listeners/EmailUsersAboutSeriesCreated.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Mail\SeriesCreated;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Support\Facades\Mail;
+
+class EmailUsersAboutSeriesCreated
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     *
+     * @param  object  $event
+     * @return void
+     */
+    public function handle($event)
+    {
+        $userList = User::all();
+
+        foreach ($userList as $index => $user) {
+            $email = new SeriesCreated(
+                $event->seriesName,
+                $event->serieId,
+                $event->seariesSeasonsQty,
+                $event->seriesEpisodesPerSeason
+            );
+
+            $when = now()->addSeconds($index * 5);
+            Mail::to($user)->later($when, $email);
+        }
+    }
+}
