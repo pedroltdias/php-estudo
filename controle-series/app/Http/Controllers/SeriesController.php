a8c45a90a0f3ec3ef62139f0db32995799619a27
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-07-28 15:03:04 -0300

    recebendo e salvando imagem ao criar serie

--- controle-series/app/Http/Controllers/SeriesController.php
+++ controle-series/app/Http/Controllers/SeriesController.php
@@ -34,7 +34,9 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        dd($request->file('cover'));
+        $coverPath = $request->file('cover')
+            ->store('series_cover', 'public');
+        $request->coverPath = $coverPath;
         $serie = $this->repository->add($request);
         \App\Events\SeriesCreated::dispatch(
             $serie->nome,
