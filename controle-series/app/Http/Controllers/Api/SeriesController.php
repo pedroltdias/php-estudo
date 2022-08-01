6f577c7c236220a993483809c68a8212e612d5c7
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-01 13:34:59 -0300

    utilizando repositorio de series na controller

--- controle-series/app/Http/Controllers/Api/SeriesController.php
+++ controle-series/app/Http/Controllers/Api/SeriesController.php
@@ -5,9 +5,14 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\SeriesFormRequest;
 use App\Models\Series;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(private SeriesRepository $seriesRepository)
+    {
+    }
+
     public function index()
     {
         return Series::all();
@@ -16,6 +21,6 @@ class SeriesController extends Controller
     public function store(SeriesFormRequest $request)
     {
         return response()
-            ->json(Series::create($request->all()), 201);
+            ->json($this->seriesRepository->add($request), 201);
     }
 }
