39a22f494912a466e101b560feac51bdb63e367e
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-01 15:18:23 -0300

    fazendo um cast utilizando acessors

--- controle-series/app/Models/Episode.php
+++ controle-series/app/Models/Episode.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,5 +15,13 @@ class Episode extends Model
     public function season()
     {
         return $this->belongsTo(Season::class);
+    }
+
+    protected function watched(): Attribute
+    {
+        return new Attribute(
+            get: fn ($watched) => (bool) $watched,
+            set: fn ($watched) => (bool) $watched,
+        );
     }
 }
