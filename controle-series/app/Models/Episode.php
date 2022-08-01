0550f428dd9bb10718befa4ef3a86a334cc3b696
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-01 15:19:41 -0300

    forma mais simples de fazer cast

--- controle-series/app/Models/Episode.php
+++ controle-series/app/Models/Episode.php
@@ -11,17 +11,12 @@ class Episode extends Model
     use HasFactory;
     public $timestamps = false;
     protected $fillable = ['number'];
+    protected $casts = [
+        'watched' => 'boolean'
+    ];
 
     public function season()
     {
         return $this->belongsTo(Season::class);
-    }
-
-    protected function watched(): Attribute
-    {
-        return new Attribute(
-            get: fn ($watched) => (bool) $watched,
-            set: fn ($watched) => (bool) $watched,
-        );
     }
 }
