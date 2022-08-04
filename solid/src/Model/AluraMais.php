7370fcb45da7016911a685db8350a496bdd8bf8c
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-04 14:24:08 -0300

    resolvendo quebra de contrato baseada na implementacao da classe AluraMais

--- solid/src/Model/AluraMais.php
+++ solid/src/Model/AluraMais.php
@@ -14,7 +14,7 @@ class AluraMais extends Video implements Pontuavel
 
     public function recuperarUrl(): string
     {
-        return str_replace(' ', '-', strtolower($this->categoria));
+        return 'http://videos.alura.com.br/' . str_replace(' ', '-', strtolower($this->categoria));
     }
 
     public function recuperarPontuacao(): int
