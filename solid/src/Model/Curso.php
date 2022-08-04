fb53754bd97df40feef353662d74ee29ed057ebe
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-04 13:36:00 -0300

    extraindo funcoes de feedback da classe curso para a classe feedback

--- solid/src/Model/Curso.php
+++ solid/src/Model/Curso.php
@@ -15,13 +15,9 @@ class Curso
         $this->feedbacks = [];
     }
 
-    public function receberFeedback(int $nota, ?string $depoimento): void
+    public function receberFeedback(Feedback $feedback): void
     {
-        if ($nota < 9 && empty($depoimento)) {
-            throw new \DomainException('Depoimento obrigatório');
-        }
-
-        $this->feedbacks[] = [$nota, $depoimento];
+        $this->feedbacks[] = $feedback;
     }
 
     public function adicionarVideo(Video $video)
--- solid/src/Model/Feedback.php
+++ solid/src/Model/Feedback.php
@@ -4,5 +4,16 @@ namespace Alura\Solid\Model;
 
 class Feedback
 {
+    private int $nota;
+    private ?string $depoimento;
 
+    public function __construct(int $nota, ?string $depoimento)
+    {
+        if ($nota < 9 && empty($depoimento)) {
+            throw new \DomainException('Depoimento obrigatório');
+        }
+
+        $this->nota = $nota;
+        $this->depoimento = $depoimento;
+    }
 }
