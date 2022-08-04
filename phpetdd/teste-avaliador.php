c93bc6eee1dd5ec70383d19617397bbeca7a4862
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-04 16:29:50 -0300

    criacao de um 'teste'

---
+++ phpetdd/teste-avaliador.php
@@ -0,0 +1,22 @@
+<?php
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+
+require 'vendor/autoload.php';
+
+$leilao = new Leilao('Fiat 147 0km');
+
+$maria = new Usuario('Maria');
+$joao = new Usuario('Joao');
+
+$leilao->recebeLance(new Lance($joao, 2000));
+$leilao->recebeLance(new Lance($maria, 2500));
+
+$leiloeiro = new Avaliador();
+$leiloeiro->avalia($leilao);
+$maiorValor = $leiloeiro->getMaiorValor();
+
+echo $maiorValor;
