a203fb1003f33bff156017d45b31c3afb7ed6038
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-05 09:05:02 -0300

    criacao do primeiro teste usando phpUnit

--- phpetdd/teste-avaliador.php
+++ phpetdd/teste-avaliador.php
@@ -7,6 +7,7 @@ use Alura\Leilao\Service\Avaliador;
 
 require 'vendor/autoload.php';
 
+// Arrange - Given / Preparamos o cenário do teste
 $leilao = new Leilao('Fiat 147 0km');
 
 $maria = new Usuario('Maria');
@@ -16,12 +17,16 @@ $leilao->recebeLance(new Lance($joao, 2000));
 $leilao->recebeLance(new Lance($maria, 2500));
 
 $leiloeiro = new Avaliador();
+
+// Act - When / Executamos o código a ser testado
 $leiloeiro->avalia($leilao);
+
 $maiorValor = $leiloeiro->getMaiorValor();
 
+// Assert - Then / Verificamos se a saída é a esperada
 $valorEsperado = 2500;
 
-if ($maiorValor === $valorEsperado) {
+if ($maiorValor == $valorEsperado) {
     echo "TESTE OK";
 } else {
     echo "TESTE FALHOU";
---
+++ phpetdd/tests/Service/AvaliadorTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Alura\Leilao\Tests\Service;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use Alura\Leilao\Service\Avaliador;
+use PHPUnit\Framework\TestCase;
+
+class AvaliadorTest extends TestCase
+{
+    public function testUm()
+    {
+        // Arrange - Given / Preparamos o cenário do teste
+        $leilao = new Leilao('Fiat 147 0km');
+
+        $maria = new Usuario('Maria');
+        $joao = new Usuario('Joao');
+
+        $leilao->recebeLance(new Lance($joao, 2000));
+        $leilao->recebeLance(new Lance($maria, 2500));
+
+        $leiloeiro = new Avaliador();
+
+        // Act - When / Executamos o código a ser testado
+        $leiloeiro->avalia($leilao);
+
+        $maiorValor = $leiloeiro->getMaiorValor();
+
+        // Assert - Then / Verificamos se a saída é a esperada
+        $valorEsperado = 2500;
+        $this->assertEquals(2500, $maiorValor);
+    }
+}
