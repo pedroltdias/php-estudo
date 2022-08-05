655c5f7fb32ace1b7aa2546e1fc6a55f38db8789
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-05 10:52:10 -0300

    teste para devolver os 3 maiores lances

--- phpetdd/tests/Service/AvaliadorTest.php
+++ phpetdd/tests/Service/AvaliadorTest.php
@@ -107,4 +107,28 @@ class AvaliadorTest extends TestCase
         // Assert - Then / Verificamos se a saída é a esperada
         self::assertEquals(2000, $menorValor);
     }
+
+    public function testAvaliadorDeveBuscar3MaioresValores()
+    {
+        $leilao = new Leilao('Fiat 147 0KM');
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+        $ana = new Usuario('Ana');
+        $jorge = new Usuario('Jorge');
+
+        $leilao->recebeLance(new Lance($ana, 1500));
+        $leilao->recebeLance(new Lance($joao, 1000));
+        $leilao->recebeLance(new Lance($maria, 2000));
+        $leilao->recebeLance(new Lance($jorge, 1700));
+
+        $leiloeiro = new Avaliador();
+        $leiloeiro->avalia($leilao);
+
+        $maiores = $leiloeiro->getMaioresLances();
+
+        static::assertCount(3, $maiores);
+        static::assertEquals(2000, $maiores[0]->getValor());
+        static::assertEquals(1700, $maiores[1]->getValor());
+        static::assertEquals(1500, $maiores[2]->getValor());
+    }
 }
