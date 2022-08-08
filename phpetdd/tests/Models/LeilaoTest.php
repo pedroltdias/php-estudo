80be486076ee49dc24fadd36acc937cba2a16fe2
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-08 09:58:13 -0300

    testes do leilao criado

---
+++ phpetdd/tests/Models/LeilaoTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Alura\Leilao\Tests\Model;
+
+use Alura\Leilao\Model\Lance;
+use Alura\Leilao\Model\Leilao;
+use Alura\Leilao\Model\Usuario;
+use PHPUnit\Framework\TestCase;
+
+class LeilaoTest extends TestCase
+{
+    /**
+     * @dataProvider geraLances
+     */
+    public function testLeilaoDeveReceberLances(
+        int $qtdLances,
+        Leilao $leilao,
+        array $valores
+    )
+    {
+        static::assertCount($qtdLances, $leilao->getLances());
+        foreach ($valores as $i => $valorEsperado) {
+            static::assertEquals($valorEsperado, $leilao->getLances()[$i]->getValor());
+        }
+    }
+
+    public function geraLances()
+    {
+        $joao = new Usuario('João');
+        $maria = new Usuario('Maria');
+
+        $leilaoCom2Lances = new Leilao('Fiat 147 0KM');
+        $leilaoCom2Lances->recebeLance(new Lance($joao, 1000));
+        $leilaoCom2Lances->recebeLance(new Lance($maria, 2000));
+
+        $leilaoCom1Lance = new Leilao('Fusca 1972 0KM');
+        $leilaoCom1Lance->recebeLance(new Lance($maria, 5000));
+
+        return [
+            '2-lances' => [2, $leilaoCom2Lances, [1000, 2000]],
+            '1-lance' => [1, $leilaoCom1Lance, [5000]]
+        ];
+    }
+}
