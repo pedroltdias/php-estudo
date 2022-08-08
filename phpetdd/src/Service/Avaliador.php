5d46ae51c00c7872f2f4c0edb2a9a4790e6380c0
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-08 11:16:38 -0300

    inicio do curso de mocks

--- phpetdd/src/Service/Avaliador.php
+++ phpetdd/src/Service/Avaliador.php
@@ -7,19 +7,16 @@ use Alura\Leilao\Model\Leilao;
 
 class Avaliador
 {
-    private float $maiorValor = -INF;
-    private float $menorValor = +INF;
-    private array $maioresLances;
+    /** @var float */
+    private $menorValor = INF;
+    /** @var float */
+    private $maiorValor = 0;
+    /** @var Lance[]|array */
+    private $maiores;
 
-    public function avalia(Leilao $leilao): void
+    public function avalia(Leilao $leilao)
     {
-        if ($leilao->estaFinalizado()) {
-            throw new \DomainException('Leilão já finalizado');
-        }
-
-        if (empty($leilao->getLances())) {
-            throw new \DomainException('Não é possível avaliar leilão vazio');
-        }
+        $leilao->finaliza();
 
         foreach ($leilao->getLances() as $lance) {
             if ($lance->getValor() > $this->maiorValor) {
@@ -29,18 +26,9 @@ class Avaliador
             if ($lance->getValor() < $this->menorValor) {
                 $this->menorValor = $lance->getValor();
             }
+
+            $this->maiores = $this->avaliaTresMaioresLances($leilao);
         }
-
-        $lances = $leilao->getLances();
-        usort($lances, function(Lance $lance1, Lance $lance2) {
-            return $lance2->getValor() - $lance1->getValor();
-        });
-        $this->maioresLances = array_slice($lances, 0, 3);
-    }
-
-    public function getMaiorValor(): float
-    {
-        return $this->maiorValor;
     }
 
     public function getMenorValor(): float
@@ -48,8 +36,30 @@ class Avaliador
         return $this->menorValor;
     }
 
-    public function getMaioresLances(): array
+    public function getMaiorValor(): float
     {
-        return $this->maioresLances;
+        return $this->maiorValor;
+    }
+
+    /**
+     * @return Lance[]
+     */
+    public function getTresMaioresLances(): array
+    {
+        return $this->maiores;
+    }
+
+    /**
+     * @param Leilao $leilao
+     * @return Lance[]|array
+     */
+    private function avaliaTresMaioresLances(Leilao $leilao)
+    {
+        $lances = $leilao->getLances();
+        usort($lances, function (Lance $lance1, Lance $lance2) {
+            return $lance2->getValor() - $lance1->getValor();
+        });
+
+        return array_slice($lances, 0, 3);
     }
 }
