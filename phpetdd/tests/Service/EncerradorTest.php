17751dbeb571a1ab9793aba3ce1c9e11a9ad914a
Pedro Dias <78484300+pedroltdias@users.noreply.github.com>
2022-08-08 13:52:20 -0300

    utilizando a Mock do PHPUnit

--- phpetdd/tests/Service/EncerradorTest.php
+++ phpetdd/tests/Service/EncerradorTest.php
@@ -7,34 +7,6 @@ use Alura\Leilao\Model\Leilao;
 use Alura\Leilao\Service\Encerrador;
 use PHPUnit\Framework\TestCase;
 
-class LeilaoDaoMock extends LeilaoDao
-{
-    private array $leiloes;
-
-    public function salva(Leilao $leilao): void
-    {
-        $this->leiloes[] = $leilao;
-    }
-
-    public function recuperarNaoFinalizados(): array
-    {
-        return array_filter($this->leiloes, function (Leilao $leilao) {
-            return !$leilao->estaFinalizado();
-        });
-    }
-
-    public function recuperarFinalizados(): array
-    {
-        return array_filter($this->leiloes, function (Leilao $leilao) {
-            return $leilao->estaFinalizado();
-        });
-    }
-
-    public function atualiza(Leilao $leilao)
-    {
-    }
-}
-
 class EncerradorTest extends TestCase
 {
     public function testLeiloesComMaisDeUmaSemanaDevemSerEncerrados()
@@ -49,7 +21,7 @@ class EncerradorTest extends TestCase
             new \DateTimeImmutable('10 days ago')
         );
 
-        $leilaoDao = new LeilaoDaoMock();
+        $leilaoDao = $this->createMock(LeilaoDao::class);
         $leilaoDao->salva($fiat147);
         $leilaoDao->salva($variant);
 
@@ -58,6 +30,8 @@ class EncerradorTest extends TestCase
 
         $leiloes = $leilaoDao->recuperarFinalizados();
         self::assertCount(2, $leiloes);
+        self::assertTrue($leiloes[0]->estaFinalizado());
+        self::assertTrue($leiloes[1]->estaFinalizado());
         self::assertEquals(
             'Fiat 147 0km',
             $leiloes[0]->recuperarDescricao()
